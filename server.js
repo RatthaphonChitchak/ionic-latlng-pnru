59156006cd0649d44fc1400e132a7a2112a501eb
Ratthaphon <toy_2537@msn.com>
2020-12-11 19:53:17 +0700

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const app = require('./src/config/app');
 
-app.listen(process.env.PORT || 5000, function () {
+app.listen(process.env.PORT || 2000, function () {
     console.log('Start server');
     console.log('Authen service is running');
 });
