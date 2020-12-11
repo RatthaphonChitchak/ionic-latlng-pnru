0a4435bae78aa312a5956298f72d58ad03b127a3
Ratthaphon <toy_2537@msn.com>
2020-12-11 18:23:16 +0700

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const app = require('./src/config/app');
 
-app.listen(3000, function () {
+app.listen(3001, function () {
     console.log('Start server');
     console.log('Service is running');
 });
